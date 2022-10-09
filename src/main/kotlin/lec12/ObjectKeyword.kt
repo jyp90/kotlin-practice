0b1de547a51f420e63e8ea6130a1cb7bf042660a
jypark <babungv@gmail.com>
2022-10-09 12:01:25 +0900

    Lecture 11 Object Keyword (static)

---
+++ src/main/kotlin/lec12/ObjectKeyword.kt
@@ -0,0 +1,20 @@
+package lec12
+
+// static : 클래스가 인스턴스화 할 때 새로운 값이 복제되는게 아니라 정적으로 인스턴스끼리의 값을 공유
+// companion object : 클래스와 동행하는 유일한 오브젝트
+class Person private constructor(
+    var name: String,
+    var age: Int,
+) {
+    // static
+    companion object {
+        private const val MIN_AGE = 1
+        fun newBaby(name: String): Person {
+            return Person(name, MIN_AGE)
+        }
+    }
+}
+
+// singleton
+
+// object
