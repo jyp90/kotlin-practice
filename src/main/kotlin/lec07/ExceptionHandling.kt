197f10de5bdb5d4bfc8c98eb40154fd94500568f
jypark <babungv@gmail.com>
2022-10-09 10:36:03 +0900

    Finish of Lecture 07 (Try Catch Exception)

---
+++ src/main/kotlin/lec07/ExceptionHandling.kt
@@ -0,0 +1,40 @@
+package lec07
+
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileReader
+import java.lang.IllegalArgumentException
+import java.lang.NumberFormatException
+
+// try catch finally
+fun parseIntOrThrow(str: String): Int {
+    try {
+        return str.toInt()
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("$e is not number")
+    }
+}
+
+fun parseIntOrThrowV02(str: String): Int? {
+    return try { // using expression
+        str.toInt()
+    } catch (e: NumberFormatException) {
+        null
+    }
+}
+
+// unchecked exception
+fun readFile() {
+    val currentFile = File(".")
+    val file = File(currentFile.absoluteFile + "/a.txt")
+    val reader = BufferedReader(FileReader(file))
+    println(reader.readLine())
+    reader.close()
+}
+
+// try with resources
+fun readFileWithPath(path: String) {
+    BufferedReader(FileReader(path)).use {reader ->
+        println(reader.readLine())
+    }
+}
