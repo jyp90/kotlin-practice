3366991bcb55eee6fe38e65e1c71b677bdcdbbfb
jypark <babungv@gmail.com>
2022-10-09 10:50:41 +0900

    Lecture 08 Collection Parameter (using vararg)

--- src/main/kotlin/lec08/KtFunctions.kt
+++ src/main/kotlin/lec08/KtFunctions.kt
@@ -22,10 +22,23 @@ fun main () {
     // named argument
     repeat("Hello World", useNewLine = true)
     printNameAndGender(gender = "F" , name = "jypark")
+
+    printAll("A", "B", "C")
+
+    val arrays = arrayOf("A", "B", "C")
+    printAll(*arrays) // using * for array
 }
 
 
 fun printNameAndGender(name: String, gender: String) {
     println(name)
     println(gender)
+}
+
+// Warn : Kotlin named Argument cannot using java method
+
+// collection parameter
+fun printAll(vararg strings: String) {
+    for(str in strings)
+        println(str)
 }
