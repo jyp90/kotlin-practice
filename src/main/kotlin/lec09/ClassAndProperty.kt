cd08a4ba085dbc44d3a142403d3e313ebb7022e1
jypark <babungv@gmail.com>
2022-10-09 11:02:07 +0900

    Lecture 09 Create Constructor and Init

---
+++ src/main/kotlin/lec09/ClassAndProperty.kt
@@ -0,0 +1,31 @@
+package lec09
+
+import java.lang.IllegalArgumentException
+
+class Person( // primary constructor, never null
+    val name: String,
+    var age: Int
+) {
+    init { // initial constructor
+        if(age <= 0) {
+            throw IllegalArgumentException("$age is not less than 0")
+        }
+    }
+
+    constructor(name: String): this(name, 1) // baby creator, call default constructor
+
+    constructor(): this("defaultName") {
+        println("possible to create body")
+    }
+}
+
+fun main() {
+    val person = Person("jypark", 100)
+    println(person.name) // getter
+    println(person.age) // getter
+    person.age = 10 // setter
+    println(person.age) // 10
+
+    val baby = Person("baby")
+    val defaultPerson = Person()
+}
